632263cacb0af7ebbd9215f0d6fc1402a81fd4b5
Chris Chang <chrischang16173@gmail.com>
2026-05-19 01:15:26 -0400

    Restart live backend during override deploy

--- scripts/deploy-live-default-app-overrides.ps1
+++ scripts/deploy-live-default-app-overrides.ps1
@@ -30,6 +30,7 @@ spec:
 "@ | Set-Content -LiteralPath $patchPath -Encoding UTF8
 
 kubectl -n $AppNamespace patch deployment $BackendDeployment --type strategic --patch-file $patchPath
+kubectl -n $AppNamespace rollout restart "deployment/$BackendDeployment"
 kubectl -n $AppNamespace rollout status "deployment/$BackendDeployment" --timeout=180s
 
 if (kubectl -n $AppNamespace get deployment $FrontendDeployment 2>$null) {
